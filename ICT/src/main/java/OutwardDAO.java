b333fb11525dac0f4dfcd76a4e15539dc9b43fd1
小橋司明 <bashiko.moto@gmail.com>
2019-11-06 10:53:32 +0900

    newclassDAO SQL改良

--- ICT/src/main/java/OutwardDAO.java
+++ ICT/src/main/java/OutwardDAO.java
@@ -1,13 +1,17 @@
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalTime;
 
 @Repository
 public class OutwardDAO {
     @Autowired
     private JdbcTemplate jdbc;
 
-    public int select(entity){
-        String x = zikann();
+    public void select(){
+        //String x = zikann();
+        LocalTime x = LocalTime.now();
         var sql = "SELECT * FROM 復路時刻表　WHERE 復路時刻表.Chitose > " +x;
         var sql2 = "SELECT 復路時刻表.* ,乗り場.NORIBA FROM 復路時刻表 LEFT JOIN  乗り場 ON 復路時刻表.ID = 乗り場.ID";
 
--- ICT/src/main/java/homewardDAO.java
+++ ICT/src/main/java/homewardDAO.java
@@ -1,15 +1,19 @@
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import java.time.LocalTime;
 
 @Repository
 public class homewardDAO {
     @Autowired
     private JdbcTemplate jdbc;
 
-    public int select(entity){
-        String x = zikoku();
+    public void select(){
+       // String x = zikoku();
+        LocalTime x = LocalTime.now();
         //xの値に時刻が入る。
         var sql = "SELECT * FROM 復路時刻表　WHERE 復路時刻表.Chitose > " +x;
-        var sql2 = "SELECT 往路時刻表.* ,乗り場.NORIBA FROM 往路時刻表 LEFT JOIN  乗り場 ON 往路時刻表.ID = 乗り場.ID";
     }
+    
 }
