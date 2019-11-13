681d2b5e30af7a9d6d5723e6016a893444ae0575
小橋司明 <bashiko.moto@gmail.com>
2019-11-13 11:33:12 +0900

    newclassDAO      SQLデータに合わせて変更

--- ICT/src/main/java/OutwardDAO.java
+++ ICT/src/main/java/OutwardDAO.java
@@ -12,8 +12,8 @@ public class OutwardDAO {
     public void select(){
         //String x = zikann();
         LocalTime x = LocalTime.now();
-        var sql = "SELECT * FROM 復路時刻表　WHERE 復路時刻表.Chitose > " +x;
-        var sql2 = "SELECT 復路時刻表.* ,乗り場.NORIBA FROM 復路時刻表 LEFT JOIN  乗り場 ON 復路時刻表.ID = 乗り場.ID";
+        var sql = "SELECT * FROM 復路時刻表　WHERE 復路時刻表.CHITOSE > " +x;
+        var sql2 = "SELECT 復路時刻表.* ,IDと乗り場.NORIBA FROM 復路時刻表 LEFT JOIN  IDと乗り場 ON 復路時刻表.ID = IDと乗り場.ID";
 
 }
 
