9c00b6267016762d2331797f9cf52ce9998ab0c9
小橋司明 <bashiko.moto@gmail.com>
2019-11-20 11:37:40 +0900

    sql変更

--- ICT/src/main/java/OutwardDAO.java
+++ ICT/src/main/java/OutwardDAO.java
@@ -1,21 +1,23 @@
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Controller;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalTime;
 
+import static org.springframework.jdbc.core.BeanPropertyRowMapper.newInstance;
+
 @Repository
 public class OutwardDAO {
+
     @Autowired
     private JdbcTemplate jdbc;
 
-    public void select(){
+     public Object select(){
         //String x = zikann();
         LocalTime x = LocalTime.now();
-        var sql = "SELECT 復路時刻表.* ,IDと乗り場.NORIBA FROM 復路時刻表 LEFT JOIN  IDと乗り場 ON 復路時刻表.ID = IDと乗り場.ID WHERE 復路時刻表.CHITOSE > \" +x;";
-
-}
-
-
-
+        //String sql = "SELECT 復路時刻表.ID,IDと乗り場.NORIBA FROM 復路時刻表 LEFT JOIN  IDと乗り場 ON 復路時刻表.ID = IDと乗り場.ID WHERE 復路時刻表.HONBUTOU > " +x;
+         var sql = "SELECT * FROM 復路時刻表 WHERE 復路時刻表.HONBUTOU > '14:00:00'";
+         return jdbc.queryForObject(sql,newInstance(a.class));
+    }
 }
